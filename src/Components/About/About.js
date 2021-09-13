984d02cabf2b5e407138ac1a9bcdb0e9428dd877
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2021-09-14 02:13:02 +0530

    updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,16 +4,26 @@ import Background from './asset/gdsc.svg'
 
 const About = () => {
   return (
-    <div className="container">
-      <style>
-        @import url('https://fonts.googleapis.com/css2?family=Glory:ital,wght@1,100&family=Poppins&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Gothic+A1&display=swap');
-      </style>
-      <h1 className="heading">About</h1>
-      <span className="line"></span>
-      <img className="background" src={Background} style={{ marginLeft: "-12rem" }} height="900" width="900" alt="gdse" />
-      <p className="content">The Developer Students Club - CIEM brings to you Synthax 2.0, an open-source program to introduce students to the world of open-source development and demonstrate the power of real-time unified problem-solving. The projects that we will be hosting have been hand-picked to inspire creative thinking and collaboration among all participants.</p>
-    </div>
+    <>
+      <div className="container">
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Glory:ital,wght@1,100&family=Poppins&display=swap');
+          @import url('https://fonts.googleapis.com/css2?family=Gothic+A1&display=swap');
+        </style>
+        <div className="head">
+          <h1 className="heading">About</h1>
+          <hr id="line" ></hr>
+        </div>
+      </div>
+      <div className="container1">
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Glory:ital,wght@1,100&family=Poppins&display=swap');
+          @import url('https://fonts.googleapis.com/css2?family=Gothic+A1&display=swap');
+        </style>
+        <img src={Background} style={{marginBottom:"4rem"}} height="600" alt="gdse" />
+        <p className="content">The Developer Students Club - CIEM brings to you Synthax 2.0, an open-source program to introduce students to the world of open-source development and demonstrate the power of real-time unified problem-solving. The projects that we will be hosting have been hand-picked to inspire creative thinking and collaboration among all participants.</p>
+      </div>
+    </>
   )
 }
 
