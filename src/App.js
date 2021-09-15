bef8e9392d1566167a9b7515249721d06ce51639
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-09-15 14:19:01 +0530

    Sample

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/Banner/Banner'
 import Teams from './Components/Teams/teams'
 import About from './Components/About/About'
+import Speaker from "./Components/Speaker/Speaker";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <About />
       <Teams />
+      {/* <Speaker /> */}
 
 
     </div>
