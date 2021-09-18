5a3a67f2865c07864fb0e11080a9082553c98845
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-09-18 08:42:08 +0530

    Speaker data added

--- src/Components/Speaker/Speaker.js
+++ src/Components/Speaker/Speaker.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import linkedin from './images/linkedin.png'
 import github from './images/github.png'
-import ananya from './images/Ananya.jpg'
+import Speaker_data from './Team.json'
 import './Speaker.css'
 
 function Speaker() {
@@ -13,283 +13,50 @@ function Speaker() {
                     <hr />
                 </h1>
             </div>
-
             <div className="Speakers__first-row">
-                <div className="Speakers__card">
-                    <div className="Speakers__profile-pic-container">
-                        <img
-                            src="https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg"
-                            alt="Avatar"
-                            className="Speakers__profile-pic"
-                        />
-                    </div>
-                    <div className="Speakers__container">
-                        <h4>
-                            <b>Dummy File</b>
-                        </h4>
-                        <p>GDSC Lead</p>
-                        <div className="Speakers__social-media">
-                            <a
-                                href=""
-                                alt="Dummy Linkedin"
-                            >
-                                <img
-                                    src={linkedin}
-                                    alt="Dummy Linkedin"
-                                    className="Speakers__linkedin"
-                                ></img>
-                            </a>
-                            <a href="">
-                                <img
-                                    src={github}
-                                    alt="Dummy Github"
-                                    className="Speakers__github"
-                                ></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="Speakers__card">
-                    <div className="Speakers__profile-pic-container">
-                        <img
-                            src="https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg"
-                            alt="Avatar"
-                            className="Speakers__profile-pic"
-                        />
-                    </div>
-                    <div className="Speakers__container">
-                        <h4>
-                            <b>Dummy File</b>
-                        </h4>
-                        <p>GDSC Lead</p>
-                        <div className="Speakers__social-media">
-                            <a
-                                href=""
-                                alt="Dummy Linkedin"
-                            >
-                                <img
-                                    src={linkedin}
-                                    alt="Dummy Linkedin"
-                                    className="Speakers__linkedin"
-                                ></img>
-                            </a>
-                            <a href="">
-                                <img
-                                    src={github}
-                                    alt="Dummy Github"
-                                    className="Speakers__github"
-                                ></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="Speakers__card">
-                    <div className="Speakers__profile-pic-container">
-                        <img
-                            src="https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg"
-                            alt="Avatar"
-                            className="Speakers__profile-pic"
-                        />
-                    </div>
-                    <div className="Speakers__container">
-                        <h4>
-                            <b>Dummy File</b>
-                        </h4>
-                        <p>GDSC Lead</p>
-                        <div className="Speakers__social-media">
-                            <a
-                                href=""
-                                alt="Dummy Linkedin"
-                            >
-                                <img
-                                    src={linkedin}
-                                    alt="Dummy Linkedin"
-                                    className="Speakers__linkedin"
-                                ></img>
-                            </a>
-                            <a href="">
-                                <img
-                                    src={github}
-                                    alt="Dummy Github"
-                                    className="Speakers__github"
-                                ></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="Speakers__card">
-                    <div className="Speakers__profile-pic-container">
-                        <img
-                            src="https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg"
-                            alt="Avatar"
-                            className="Speakers__profile-pic"
-                        />
-                    </div>
-                    <div className="Speakers__container">
-                        <h4>
-                            <b>Dummy File</b>
-                        </h4>
-                        <p>GDSC Lead</p>
-                        <div className="Speakers__social-media">
-                            <a
-                                href=""
-                                alt="Dummy Linkedin"
-                            >
-                                <img
-                                    src={linkedin}
-                                    alt="Dummy Linkedin"
-                                    className="Speakers__linkedin"
-                                ></img>
-                            </a>
-                            <a href="">
-                                <img
-                                    src={github}
-                                    alt="Dummy Github"
-                                    className="Speakers__github"
-                                ></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {Speaker_data.map((speaker_detail, index) => {
+                    return (
+                        <div className="Speakers__card">
+                            <div className="Speakers__profile-pic-container">
 
-            </div>
-            <div className="Speakers__first-row">
-                <div className="Speakers__card">
-                    <div className="Speakers__profile-pic-container">
-                        <img
-                            src="https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg"
-                            alt="Avatar"
-                            className="Speakers__profile-pic"
-                        />
-                    </div>
-                    <div className="Speakers__container">
-                        <h4>
-                            <b>Dummy File</b>
-                        </h4>
-                        <p>GDSC Lead</p>
-                        <div className="Speakers__social-media">
-                            <a
-                                href=""
-                                alt="Dummy Linkedin"
-                            >
                                 <img
-                                    src={linkedin}
-                                    alt="Dummy Linkedin"
-                                    className="Speakers__linkedin"
-                                ></img>
-                            </a>
-                            <a href="">
-                                <img
-                                    src={github}
-                                    alt="Dummy Github"
-                                    className="Speakers__github"
-                                ></img>
-                            </a>
+                                    src={speaker_detail.imgUrl}
+                                    alt="Avatar"
+                                    className="Speakers__profile-pic"
+                                />
+                            </div>
+                            <div className="Speakers__container">
+                                <h4>
+                                    <b>{speaker_detail.name}</b>
+                                </h4>
+                                <p>{speaker_detail.position}</p>
+                                <div className="Speakers__social-media">
+                                    <a
+                                        href=""
+                                        alt="Dummy Linkedin"
+                                    >
+                                        <img
+                                            src={linkedin}
+                                            alt="Dummy Linkedin"
+                                            className="Speakers__linkedin"
+                                        ></img>
+                                    </a>
+                                    <a href="">
+                                        <img
+                                            src={github}
+                                            alt="Dummy Github"
+                                            className="Speakers__github"
+                                        ></img>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="Speakers__card">
-                    <div className="Speakers__profile-pic-container">
-                        <img
-                            src="https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg"
-                            alt="Avatar"
-                            className="Speakers__profile-pic"
-                        />
-                    </div>
-                    <div className="Speakers__container">
-                        <h4>
-                            <b>Dummy File</b>
-                        </h4>
-                        <p>GDSC Lead</p>
-                        <div className="Speakers__social-media">
-                            <a
-                                href=""
-                                alt="Dummy Linkedin"
-                            >
-                                <img
-                                    src={linkedin}
-                                    alt="Dummy Linkedin"
-                                    className="Speakers__linkedin"
-                                ></img>
-                            </a>
-                            <a href="">
-                                <img
-                                    src={github}
-                                    alt="Dummy Github"
-                                    className="Speakers__github"
-                                ></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="Speakers__card">
-                    <div className="Speakers__profile-pic-container">
-                        <img
-                            src="https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg"
-                            alt="Avatar"
-                            className="Speakers__profile-pic"
-                        />
-                    </div>
-                    <div className="Speakers__container">
-                        <h4>
-                            <b>Dummy File</b>
-                        </h4>
-                        <p>GDSC Lead</p>
-                        <div className="Speakers__social-media">
-                            <a
-                                href=""
-                                alt="Dummy Linkedin"
-                            >
-                                <img
-                                    src={linkedin}
-                                    alt="Dummy Linkedin"
-                                    className="Speakers__linkedin"
-                                ></img>
-                            </a>
-                            <a href="">
-                                <img
-                                    src={github}
-                                    alt="Dummy Github"
-                                    className="Speakers__github"
-                                ></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="Speakers__card">
-                    <div className="Speakers__profile-pic-container">
-                        <img
-                            src="https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg"
-                            alt="Avatar"
-                            className="Speakers__profile-pic"
-                        />
-                    </div>
-                    <div className="Speakers__container">
-                        <h4>
-                            <b>Dummy File</b>
-                        </h4>
-                        <p>GDSC Lead</p>
-                        <div className="Speakers__social-media">
-                            <a
-                                href=""
-                                alt="Dummy Linkedin"
-                            >
-                                <img
-                                    src={linkedin}
-                                    alt="Dummy Linkedin"
-                                    className="Speakers__linkedin"
-                                ></img>
-                            </a>
-                            <a href="">
-                                <img
-                                    src={github}
-                                    alt="Dummy Github"
-                                    className="Speakers__github"
-                                ></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                    )
+
+                })}
+
+
+
 
             </div>
         </div>
